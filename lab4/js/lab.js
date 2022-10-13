5edac309a443f8656436fe86fd80a6d9248056ca
MintyComet <vanessasho36@gmail.com>
2022-10-12 23:07:10 -0700

    Lab 4
    Nick and my work on lab 4, pt 1

---
+++ lab4/js/lab.js
@@ -0,0 +1,28 @@
+// Summary: This file contains the pseudocode for a hypothetical library search.
+// Description: Mainly for practice in writing pseudocode, this file was created to
+// satisfy the requirements of lab 4 by creating pseudocode for a hypothetical
+// computer scenario that could be coded in sections by a programmer.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author NicholasEyestone & VanessaHo.
+// @since  10.12.2022
+
+// Prompt user to input whether they are searching by title or author.
+    // Display a drop down select box.
+    // Display text telling the user to select from the two options.
+// Prompt user to type in their search text.
+    // Display a text box for input.
+    // Display text telling the user to type their search.
+// Accept user input.
+    // Read the option selected from the drop down menu.
+    // Read the text inputted by the user in the text box.
+// Compare the input text to the library lists of the type selected.
+    // Evaluate whether the user chose author or title.
+    // Check if any text in the library’s data matches the text inputted by the user.
+    // Check if any information partially matches the user input.
+// Sort resulting information for display.
+    // Organize information results from closest matches to partial matches.
+    // Sort data into a vertically listed format.
+// Display information or that it is not found.
+    // If there was one or more exact matches for the search, display the organized list.
+    // If there was no exact match, display a message saying so and also the partially matching results.
