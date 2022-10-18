69deb0fa970408e10c8872296cfdb97458faa5ca
MintyComet <vanessasho36@gmail.com>
2022-10-17 22:00:20 -0700

    Lab 5
    Mine and Nick's work on lab 5, should be the first and last one.

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/**
+ * Author:    Vanessa Ho <vsho@ucsc.edu> and Nick Eyestone <neyeston@ucsc.edu>
+ * Created:   17 October 2022
+ **/
+
+// Define Variables
+
+var make = "Delorean";
+var model = "DMC-12";
+var color = "Gray";
+var year = 1982;
+var ownIt = false;
+
+// Calculate
+var age = 2022 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+document.writeln("Own It: " + ownIt +"<br>");
