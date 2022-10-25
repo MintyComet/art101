af0ca119159b9a34b6096405758ab03c5218bb0c
MintyComet <vanessasho36@gmail.com>
2022-10-24 22:12:13 -0700

    Lab 7 Bonus
    Bonus task push for lab 7.

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,9 +5,9 @@
 
 // sortName - a function that takes user input and sorts the letters
 // of their name
+var userName = window.prompt("Hi! Please tell me your name so I can fix it.");
 
-function sortName() {
-  var userName = window.prompt("Hi! Please tell me your name so I can fix it.");
+function sortName(userName) {
   console.log("userName =", userName);
   //split string to array
   var nameArray = userName.split('');
@@ -23,4 +23,4 @@ function sortName() {
 }
 
 //output
-document.writeln("Here's your fixed name: ", sortName(), "</br>");
+document.writeln("Here's your fixed name: ", sortName(userName), "</br>");
