f1c7b59a4acf3b40814ba652ef2f9066fe196088
MintyComet <vanessasho36@gmail.com>
2022-10-24 21:20:14 -0700

    Lab 7 final
    Mine and Nick's work on lab 7. Should be last push.

---
+++ lab7/js/lab.js
@@ -0,0 +1,26 @@
+/**
+ * Author:    Vanessa Ho <vsho@ucsc.edu> and Nick Eyestone <neyeston@ucsc.edu>
+ * Created:   24 October 2022
+ **/
+
+// sortName - a function that takes user input and sorts the letters
+// of their name
+
+function sortName() {
+  var userName = window.prompt("Hi! Please tell me your name so I can fix it.");
+  console.log("userName =", userName);
+  //split string to array
+  var nameArray = userName.split('');
+  console.log("nameArray =", nameArray);
+  //sort the array
+  var nameArraySort = nameArray.sort();
+  console.log("nameArraySort =", nameArraySort);
+  //join array back to a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  //return output
+  return nameSorted;
+}
+
+//output
+document.writeln("Here's your fixed name: ", sortName(), "</br>");
